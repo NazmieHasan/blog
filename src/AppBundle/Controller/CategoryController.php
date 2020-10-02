757f549557fa07b877749a9bd2ff06a7cbc302a7
NazmieHasan <58294359+NazmieHasan@users.noreply.github.com>
2020-10-02 23:46:52 +0300

    Admin - CategoryController

--- src/AppBundle/Controller/CategoryController.php
+++ src/AppBundle/Controller/CategoryController.php
@@ -2,12 +2,37 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use AppBundle\Form\CategoryType;
 
 class CategoryController extends Controller
 {
-    public function indexAction($name)
+    /**
+     * @Route("/create", name="admin_categories_create")
+     *
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function createCategory(Request $request)
     {
-        return $this->render('', array('name' => $name));
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+
+        return $this->render('admin/categories/create.html.twig',
+        ['form' => $form->createView()]);
+
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($category);
+            $em->flush();
+
+            return $this->redirectToRoute('admin_categories');
+        }
     }
 }
